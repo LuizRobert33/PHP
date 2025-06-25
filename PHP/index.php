93b637e13e37a0dcfce408a732f4be1cfe611948
Luiz Roberto <110065527+LuizRobert33@users.noreply.github.com>
2025-06-25 17:16:28 -0300

    Update

--- PHP/index.php
+++ PHP/index.php
@@ -13,6 +13,8 @@ echo "Ola Mundo \u{1F30E}"; //Pode ser usar Print tb
 //phpinfo() e uma função que diz informações do servidor
 $nome = "Luiz Roberto";
 echo "<p>Ola eu sou $nome</p>";
+echo "Hoje e dia " . date("d/m/Y") . "<br>";
+echo "e a hora atual e " . date("H:i:s") . "<br>";
 
 ?>
 
