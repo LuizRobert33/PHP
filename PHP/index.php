a3bc37f96deb193661ce06e89abca9b2889bdafa
Luiz Roberto <110065527+LuizRobert33@users.noreply.github.com>
2025-06-25 17:10:28 -0300

    Update

--- PHP/index.php
+++ PHP/index.php
@@ -6,11 +6,13 @@
     <title>Testando</title>
 </head>
 <body>
-    
+        <h1> Exemplo PHP <h1>
 <?php 
 
 echo "Ola Mundo \u{1F30E}"; //Pode ser usar Print tb
 //phpinfo() e uma função que diz informações do servidor
+$nome = "Luiz Roberto";
+echo "<p>Ola eu sou $nome</p>";
 
 ?>
 
